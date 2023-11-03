366b4ab9aa48f2ef5950df5836a2d2cba1ec56e6
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-11-03 09:09:37 -0600

    Fix: gems, feature test

--- spec/features/new_spec.rb
+++ spec/features/new_spec.rb
@@ -1,14 +1,14 @@
 require "rails_helper"
 
 RSpec.feature "User Registration", type: :feature do
-  it "User can register a new account" do
+  it "User can register a new account", :vcr do
     visit "/users/new"
 
     fill_in "Name", with: "John"
-    fill_in "Email", with: "100@example.com"
+    fill_in "Email", with: "200@example.com"
     fill_in "Password", with: "password123"
     fill_in "Password confirmation", with: "password123"
     click_button "Create New User"
-    expect(current_path).to eq("/users/6")
+    expect(current_path).to eq("/users/11")
   end
 end
