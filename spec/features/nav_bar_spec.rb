8dae6ff7fc461041c5548d15a46f098f0cba8e62
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-11-05 15:29:20 -0700

    feat: Add NavBar to Application Layout and testing

---
+++ spec/features/nav_bar_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe 'Navbar', type: :feature do
+  it 'displays the navbar and links' do
+    WebMock.allow_net_connect! 
+    WebMock.disable! 
+    VCR.eject_cassette 
+    VCR.turn_off!(:ignore_cassettes => true)
+
+    visit "/" 
+    
+    expect(page).to have_css('nav.navbar')
+    
+    within('nav.navbar') do
+      expect(page).to have_link('Flick Pick')
+    end
+
+    within('ul.navbar-nav') do
+      expect(page).to have_link('Sign in')
+      expect(page).to have_link('Create User')
+    end
+  end
+end
