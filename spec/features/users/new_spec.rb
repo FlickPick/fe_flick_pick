bda0e34d47080b8a102b9d7a376ff0bf615abd0d
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-11-08 11:36:24 -0700

    Get all tests to passing

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -4,11 +4,12 @@ RSpec.feature "User Registration", type: :feature do
   it "User can register a new account", :vcr do
     visit "/users/new"
 
-    fill_in "Name", with: "John"
-    fill_in "Email", with: "500@example.com"
+    fill_in "Name", with: "User"
+    random_email = "user_#{Time.now.to_i}@example.com"
+    fill_in "Email", with: random_email
     fill_in "Password", with: "password123"
     fill_in "Password confirmation", with: "password123"
     click_button "Create New User"
-    expect(current_path).to eq("/users/3")
+    expect(current_path).to match(/\/users\/\d+/)
   end
 end
--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -2,15 +2,13 @@ require "rails_helper"
 
 RSpec.feature "User Dashboard", type: :feature do
   it "displays the users name and Dashboard on the top of the page", :vcr do
-    visit "/users/new"
+    visit "/sessions/new"
 
-    fill_in "Name", with: "John"
-    fill_in "Email", with: "400@example.com"
-    fill_in "Password", with: "password123"
-    fill_in "Password confirmation", with: "password123"
-    click_button "Create New User"
+    fill_in "Email", with: "ethan@turing.edu"
+    fill_in "Password", with: "Ethan!12345"
+    click_button "Log in"
     expect(current_path).to eq("/users/2")
-    expect(page).to have_content("John's Dashboard")
+    expect(page).to have_content("Ethan's Dashboard")
     expect(page).to have_button("Create Party")
     expect(page).to have_content("Your Top Picks:")
   end
