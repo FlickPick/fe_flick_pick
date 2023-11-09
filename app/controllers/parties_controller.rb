368a8ace997f55a217a5b80f37b4affbc7092168
Reed Hillmar <reedhillmar@gmail.com>
2023-11-08 20:00:37 -0700

    results table working on selection page

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -30,6 +30,7 @@ class PartiesController < ApplicationController
   end
 
   def details
+    require 'pry';binding.pry
     @details = PartyFacade.new
                           .details(params[:id])             
   end
